30290c821a47285fb37149be69037ee2bcc893b6
Hernan Diaz <diazher59@gmail.com>
2023-09-15 10:16:44 -0300

    minor changes

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,5 +1,5 @@
 const routes = require('express').Router();
-const { log } = require('../config/logger');
+const log = require('../config/logger');
 const { getCompanyProducts, addCompanyProducts } = require('../controllers/products.controller');
 
 const auth = (req, res, next) => {
